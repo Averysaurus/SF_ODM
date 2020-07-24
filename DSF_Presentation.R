48cceb15535ad45d3c5704a0569d1d457da8ac42
Avery Richards <43361193+Averysaurus@users.noreply.github.com>
2020-07-24 11:28:14 -0700

    Update DSF_Presentation.R

--- DSF_Presentation.R
+++ DSF_Presentation.R
@@ -1,9 +1,10 @@
 ### Geospatial methods: example from Data Scienece Fellow presentation
-### Avery Richards, UC Berkelay School of Public Health, D-lab 
+### Avery Richards, UC Berkeley School of Public Health, D-lab 
 ### avery.richards@berkeley.edu
-### https://dlab.berkeley.edu/
 
 
+# Required packages,
+# run install.package('') as needed to load. 
 library(RSocrata)
 library(ggplot2)
 library(dplyr)
@@ -16,38 +17,38 @@ oak_calls <-
 read.socrata("https://data.oaklandnet.com/resource/quth-gb8e.json", 
                          stringsAsFactors = FALSE)
  
- #Exploring data in relevant columns.
- dim(oak_calls)
- range(oak_calls$datetimeinit)
- unique(oak_calls$reqcategory)
- unique(oak_calls$description)
+#Exploring data, dimensions and unique paramenters in columns.
+dim(oak_calls)
+range(oak_calls$datetimeinit)
+unique(oak_calls$reqcategory)
+unique(oak_calls$description)
  
- #Subetting for a day.
- oak_calls_day <- subset(oak_calls, 
+ #Subetting for a day of calls.
+oak_calls_day <- subset(oak_calls, 
                          as.data.frame.Date(datetimeinit)
                          >= "2020-04-03" & 
                            as.data.frame.Date(datetimeinit)
                          < "2020-04-04")
 
  
- home_camps_today <- oak_calls_day[oak_calls_day$description ==
+home_camps_today <- oak_calls_day[oak_calls_day$description ==
                                      "Homeless Encampment",]
  
- #Mapping with sf package.
- map_home_camps <- st_as_sf(true_home_camps_today, coords = 
+#Mapping with sf package.
+map_home_camps <- st_as_sf(true_home_camps_today, coords = 
                               c("reqaddress.longitude", 
                                 "reqaddress.latitude"), 
                             crs = 4326)
- library(tmap)
- # toggling the map mode.
- tmap_mode("view")
+library(tmap)
+# toggling the map mode to interactive.
+tmap_mode("view")
  
- #plotting map.
- qtm(map_home_camps) +
+#plotting map.
+qtm(map_home_camps) +
    tm_dots()
  
- # a random week in March.
- oak_calls_week <- subset(oak_calls, 
+# a random week in March.
+oak_calls_week <- subset(oak_calls, 
                           as.data.frame.Date(datetimeinit)
                           >= "2020-03-15" & 
                             as.data.frame.Date(datetimeinit)
@@ -69,7 +70,7 @@ read.socrata("https://data.oaklandnet.com/resource/quth-gb8e.json",
  #plotting map.
  qtm(map_home_camps_week) + tm_dots()
  
- #Monthly
+ #Monthly scale.
  oak_calls_month <- subset(oak_calls, 
                            as.data.frame.Date(datetimeinit) 
                            >= "2020-03-01" & 
@@ -77,23 +78,23 @@ read.socrata("https://data.oaklandnet.com/resource/quth-gb8e.json",
                            < "2020-04-02")
 
 # Subsetting, and grepping out those CANCEL.
- home_camps_month <- oak_calls_month[oak_calls_month$description 
+home_camps_month <- oak_calls_month[oak_calls_month$description 
                                      == "Homeless Encampment",]
- true_home_camps_month <- home_camps_month[grep("CANCEL", 
+true_home_camps_month <- home_camps_month[grep("CANCEL", 
                                                 home_camps_month$status, invert = TRUE), ]
  
 # Distinct calls, no 2+ reports per incidence.
- uni_home_camps_month = true_home_camps_month %>% 
+uni_home_camps_month = true_home_camps_month %>% 
    distinct(reqaddress.latitude,
             .keep_all = TRUE) %>% 
    distinct(reqaddress.longitude,
             .keep_all = TRUE)
  
- #Difference.
+ #the difference with distinct().
  dim(true_home_camps_month)
  dim(uni_home_camps_month)
  
- # Purge NA values.
+ # Purge NA values for map to work.
  clean_home_camps_month <- filter(uni_home_camps_month, 
                                   reqaddress.latitude != "NA" |
                                     reqaddress.longitude != "NA")
